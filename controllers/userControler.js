87efd435d58f62dcc4513495d834b693b8dec756
Eduardo_Morales <eduardo@inmo-puebla.com>
2022-01-20 22:53:02 -0600

    se sube metodo find all y create y se crea modelo de usuarios

--- controllers/userControler.js
+++ controllers/userControler.js
@@ -1,4 +1,7 @@
+const user = require("../models/users");
+
 // usado la sintaxis como crete usando la exportacion de un  objeto y dentro una funcion y asi exportarlo
+
 module.exports = {
   //! sintaxis key: value
   // =========================get all ==============
