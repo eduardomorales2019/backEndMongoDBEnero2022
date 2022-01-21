584d89af4f67bb10190ab98eac534c6f66055162
Eduardo_Morales <eduardo@inmo-puebla.com>
2022-01-20 22:06:09 -0600

    se agrega contolador, el cual se movio del  archivo index, y se creo un folder para mejor orrganizaciion

---
+++ controllers/userControler.js
@@ -0,0 +1,7 @@
+// usado la sintaxis como crete usando la exportacion de un  objeto y dentro una funcion y asi exportarlo
+module.exports = {
+  // sintaxis llave valor.
+  create: function () {
+    console.log("este va ser mi contolador");
+  },
+};
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,17 +1,19 @@
 const express = require("express"); // import express
 
 const router = express.Router(); // routes is an object
-
+// =========================
+const userContoller = require("/Users/eduardomorales/Documents/DEVF SA DE CV /KataBackMongoDB/api-mongo-express/controllers/userControler.js");
+// =========================
+// asimilando un controlador, pero lo vamos a mandar ! se define como controlador o  modulo!!
 router.get("/", (req, res, next) => {
-  res.status(200).json({ message: "all user", users: [] });
+  userContoller.create();
+  res.status(200).json({ message: "all users", users: [] });
 });
 
 router.post("/", (req, res, next) => {
-  res
-    .status(201)
-    .json({
-      message: "user created",
-      user: { name: "Eduardo", email: "eduardo@inmo-puebla.com", age: 40 },
-    });
+  res.status(201).json({
+    message: "user created",
+    user: { name: "Eduardo", email: "eduardo@inmo-puebla.com", age: 40 },
+  });
 });
 module.exports = router;
