7417c91ea1e974adb7a2fd8e59ea03fa88f9857a
Eduardo_Morales <eduardo@inmo-puebla.com>
2022-01-20 22:32:17 -0600

    creacion de rura y controlador de get all y post

--- controllers/userControler.js
+++ controllers/userControler.js
@@ -1,7 +1,15 @@
 // usado la sintaxis como crete usando la exportacion de un  objeto y dentro una funcion y asi exportarlo
 module.exports = {
-  // sintaxis llave valor.
-  create: function () {
-    console.log("este va ser mi contolador");
+  //! sintaxis key: value
+  // =========================get all ==============
+  findAll: (req, res) => {
+    res.status(201).json({ message: "all user created", users: [] });
+  },
+  // =========================POST===========================
+  create: (req, res) => {
+    res.status(201).json({
+      message: "user created",
+      user: { name: "Eduardo", email: "eduardo@inmo-puebla.com", age: 40 },
+    });
   },
 };
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,19 +1,17 @@
 const express = require("express"); // import express
+const userControler = require("/Users/eduardomorales/Documents/DEVF SA DE CV /KataBackMongoDB/api-mongo-express/controllers/userControler.js");
 
 const router = express.Router(); // routes is an object
 // =========================
 const userContoller = require("/Users/eduardomorales/Documents/DEVF SA DE CV /KataBackMongoDB/api-mongo-express/controllers/userControler.js");
 // =========================
-// asimilando un controlador, pero lo vamos a mandar ! se define como controlador o  modulo!!
-router.get("/", (req, res, next) => {
-  userContoller.create();
-  res.status(200).json({ message: "all users", users: [] });
-});
+//! Verbos HTTP
+// =========================GET===========================
+router.get("/", userContoller.findAll);
+// =========================POST===========================
 
-router.post("/", (req, res, next) => {
-  res.status(201).json({
-    message: "user created",
-    user: { name: "Eduardo", email: "eduardo@inmo-puebla.com", age: 40 },
-  });
-});
+router.post("/", userControler.create);
+// ====================================================
+
+// =========================
 module.exports = router;
