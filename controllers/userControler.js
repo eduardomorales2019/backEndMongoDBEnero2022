86e349d383b383378cd73ecbdbe488fdf7a3c06a
Eduardo_Morales <eduardo@inmo-puebla.com>
2022-01-24 15:01:37 -0600

    se agrega mas metodos de crud, y se  baja compass mongo para manjear desde ahi la base de datos.

--- controllers/userControler.js
+++ controllers/userControler.js
@@ -1,11 +1,10 @@
-const user = require("../models/users");
 const User = require("../models/users");
 
 // usado la sintaxis como crete usando la exportacion de un  objeto y dentro una funcion y asi exportarlo
 
 module.exports = {
   //! sintaxis key: value
-  // =========================get all ==============
+  // =========================find all ==============
   findAll: async (req, res) => {
     try {
       const allUsers = await User.find();
@@ -25,6 +24,66 @@ module.exports = {
       res.status(201).json({ message: "user created", user: newUser });
     } catch (err) {
       res.status(400).json({ message: "error creatig user ", error: err });
+    }
+  },
+  // ========================find one by id  ============================
+  findByID: async (req, res) => {
+    const { id } = req.params;
+    console.log(id, "id in paramas ");
+    try {
+      const userFound = await User.findById(id);
+      if (!userFound) {
+        // en lugar que sea === null
+        res.status(404).json({
+          message: "User no encontrado por el sistema ",
+          user: userFound,
+        });
+      } else {
+        res.status(200).json({ message: "User encontrado", user: userFound });
+      }
+    } catch (err) {
+      console.log(err);
+      res
+        .status(500)
+        .json({ message: "Error user not found, error", error: err });
+    }
+  },
+  // ========================Delete by id ============================
+  deleteById: async (req, res) => {
+    try {
+      const { id } = req.params.id;
+      const user = await User.findByIdAndDelete(id);
+      res.status(200).json({ message: "user deleted", user });
+    } catch (err) {
+      res
+        .status(500)
+        .json({ message: "Error al eliminar el usuario", error: err });
+    }
+  },
+  // ========================Update by id ============================
+  update: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const user = await User.updateOne(id, req.body, {
+        new: true,
+      });
+      res.status(200).json({ message: "user updated", user });
+    } catch (err) {
+      res
+        .status(500)
+        .json({ message: "Error al actualizar el usuario", error: err });
+    }
+  },
+  // ========================patch ============================
+  upDateOnebyID: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const userUpDate = await User.findByIdAndUpdate(id, req.body); //! SIEMPRE HAY QUE MANDAR EL BODY EN  UPDATE DE UN PATCH O PUT
+      res.status(200).json({ message: "user updated", userUpDate });
+    } catch (err) {
+      res
+        .status(500)
+        .json({ message: "Error al actualizar el usuario", error: err });
     }
   },
 };
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -11,7 +11,12 @@ router.get("/", userContoller.findAll);
 // =========================POST===========================
 
 router.post("/", userControler.create);
-// ====================================================
+// ========================find one============================
+router.get("/:id", userControler.findByID);
+// ========================delete Id============================
+router.delete("/:id", userControler.deleteById);
+// ========================uopdate one OR BULK (QUE SON MUCHOS, SE HACE UNA ACTUALIZACION, ejempo zentra 2016 y precios diferentes, bulk, se manda en un arreglo bulk[id.id2] , price , 2000) ============================
+router.patch("/:id", userControler.upDateOnebyID);
 
 // =========================
 module.exports = router;
